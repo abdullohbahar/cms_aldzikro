18a7d400f78d2fd8ff8eeeb9cd3ee8c443eab8ca
abdullohbahar <abdullohbahar@gmail.com>
2026-03-09 21:19:07 +0700

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\ServiceProvider;
 use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -57,5 +58,9 @@ class AppServiceProvider extends ServiceProvider
 
         // Use Tailwind pagination
         \Illuminate\Pagination\Paginator::defaultView('pagination::tailwind');
+
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
